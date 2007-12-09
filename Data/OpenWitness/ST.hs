295bfb169eb8afc9f0a494a481ae1bf15df833a2
Ashley Yakeley <ashley@semantic.org>
2007-12-09 02:03:53 -0800

    add ST
    darcs-hash:20071209100353-ac6dd-54c820d83f2857a1fd565625b6b8fce5f92ea26e

---
+++ Data/OpenWitness/ST.hs
@@ -0,0 +1,63 @@
+{-# OPTIONS -fglasgow-exts -Wall -Werror #-}
+module Data.OpenWitness.ST
+(
+	ST,runST,fixST,
+	newSTRef,readSTRef,writeSTRef,modifySTRef,
+	stToOW,RealWorld,stToIO
+) where
+{
+	import Data.OpenWitness;
+	import Data.OpenWitness.WitnessDict;
+	import Control.Monad.State;
+	
+	newtype ST s a = MkST (StateT (OpenDict s) (OW s) a) deriving (Functor,Monad,MonadFix);
+
+	stToOW :: ST s a -> OW s a;
+	stToOW (MkST st) = evalStateT st emptyOpenDict;
+
+	runST :: (forall s . ST s a) -> a;
+	runST st = runOW (stToOW st);
+	
+	fixST :: (a -> ST s a) -> ST s a;
+	fixST = mfix;
+
+	newtype STRef s a = MkSTRef (OpenKey s a) deriving Eq;
+	
+	newSTRef :: a -> ST s (STRef s a);
+	newSTRef a = MkST (do
+	{
+		wit <- lift newOpenWitnessOW;
+		dict <- get;
+		let {(dict',key) = openKeyNew wit a dict;};
+		put dict';
+		return (MkSTRef key);
+	});
+	
+	readSTRef :: STRef s a -> ST s a;
+	readSTRef (MkSTRef key) = MkST (do
+	{
+		dict <- get;
+		case openKeyLookup key dict of
+		{
+			Just a -> return a;
+			_ -> fail "ref not found";
+		};
+	});
+	
+	writeSTRef :: forall s a. STRef s a -> a -> ST s ();
+	writeSTRef (MkSTRef key) newa = MkST (modify (\dict -> case openKeyReplace key newa dict of
+	{
+		Just dict' -> dict';
+		_ -> error "ref not found";
+	}));
+	
+	modifySTRef :: forall s a. STRef s a -> (a -> a) -> ST s ();
+	modifySTRef (MkSTRef key) amap = MkST (modify (\dict -> case openKeyModify key amap dict of
+	{
+		Just dict' -> dict';
+		_ -> error "ref not found";
+	}));
+	
+	stToIO :: ST RealWorld a -> IO a;
+	stToIO = owToIO . stToOW;
+}
