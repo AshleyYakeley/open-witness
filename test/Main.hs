5817a0ee96ace8237c9c4b334e3a1359a2b93a3d
Ashley Yakeley <ashley@yakeley.org>
2017-02-01 20:33:54 -0800

    test: fix warnings

--- test/Main.hs
+++ test/Main.hs
@@ -1,46 +1,46 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 {
-	import Data.OpenWitness.ST;
-	import Data.OpenWitness.Exception;
-	import Prelude hiding (catch);
+    import Data.OpenWitness.ST;
+    import Data.OpenWitness.Exception;
+    import Prelude;
 
-	stuff :: ST s [Int];
-	stuff = do
-	{
-		ra <- newSTRef 3;
-		a1 <- readSTRef ra;
-		rb <- newSTRef 4;
-		b1 <- readSTRef rb;
-		writeSTRef ra 5;
-		a2 <- readSTRef ra;
-		writeSTRef rb 7;
-		b2 <- readSTRef rb;
-		return [a1,b1,a2,b2];
-	};
+    stuff :: ST s [Int];
+    stuff = do
+    {
+        ra <- newSTRef 3;
+        a1 <- readSTRef ra;
+        rb <- newSTRef 4;
+        b1 <- readSTRef rb;
+        writeSTRef ra 5;
+        a2 <- readSTRef ra;
+        writeSTRef rb 7;
+        b2 <- readSTRef rb;
+        return [a1,b1,a2,b2];
+    };
 
-	intExn :: Exn Int;
-	intExn = $(declexn [t|Int|]);
-	stringExn :: Exn String;
-	stringExn = $(declexn [t|String|]);
+    intExn :: Exn Int;
+    intExn = $(declexn [t|Int|]);
+    stringExn :: Exn String;
+    stringExn = $(declexn [t|String|]);
 
-	showCatch :: IO a -> IO ();
-	showCatch f = ((do
-	{
-		_ <- f;
-		putStrLn "not caught";
-	}
-	`catch` intExn) (\i -> putStrLn ("caught intExn " ++ (show i)))
-	`catch` stringExn) (\s -> putStrLn ("caught stringExn " ++ (show s)));
+    showCatch :: IO a -> IO ();
+    showCatch f = ((do
+    {
+        _ <- f;
+        putStrLn "not caught";
+    }
+    `catch` intExn) (\i -> putStrLn ("caught intExn " ++ (show i)))
+    `catch` stringExn) (\s -> putStrLn ("caught stringExn " ++ (show s)));
 
-	main :: IO ();
-	main = do
-	{
-		putStrLn (show (runST stuff));
-		showCatch (return "hello");
-		showCatch (throw intExn 3);
-		showCatch (throw stringExn "text");
-		showCatch (throw intExn 67);
-		showCatch (throw stringExn "str");
-	};
+    main :: IO ();
+    main = do
+    {
+        putStrLn (show (runST stuff));
+        showCatch (return "hello");
+        showCatch (throw intExn 3);
+        showCatch (throw stringExn "text");
+        showCatch (throw intExn 67);
+        showCatch (throw stringExn "str");
+    };
 }
