9bab099197129679258318d571846df531a31ab3
Ashley Yakeley <ashley@yakeley.org>
2017-08-02 01:47:45 -0700

    fix for GHC 8.2.1; resolvers for Travis

--- test/Object.hs
+++ test/Object.hs
@@ -1,24 +1,24 @@
 module Object where
 {
-	import Data.Witness;
-	import Data.OpenWitness;
+    import Data.Witness;
+    import Data.OpenWitness;
 
-	data Object s = forall a. MkObject (OpenWitness s a) a;
+    data Object s = forall a. MkObject (OpenWitness s a) a;
 
-	matchObject :: OpenWitness s b -> Object s -> Maybe b;
-	matchObject uqb (MkObject uqa a) = do
-	{
-		MkEqualType <- testEquality uqa uqb;
-		return a;
-	};
+    matchObject :: OpenWitness s b -> Object s -> Maybe b;
+    matchObject uqb (MkObject uqa a) = do
+    {
+        Refl <- testEquality uqa uqb;
+        return a;
+    };
 
-	makeConversions :: OpenWitness s b -> (b -> Object s,Object s -> Maybe b);
-	makeConversions wit = (MkObject wit,matchObject wit);
+    makeConversions :: OpenWitness s b -> (b -> Object s,Object s -> Maybe b);
+    makeConversions wit = (MkObject wit,matchObject wit);
 
-	getConversions :: OW s (b -> Object s,Object s -> Maybe b);
-	getConversions = do
-	{
-		wit <- newOpenWitnessOW;
-		return (makeConversions wit);
-	};
+    getConversions :: OW s (b -> Object s,Object s -> Maybe b);
+    getConversions = do
+    {
+        wit <- newOpenWitnessOW;
+        return (makeConversions wit);
+    };
 }
