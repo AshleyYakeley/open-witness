f683ae8ebc5304065b9a236048c8c5e5a64db830
Ashley Yakeley <ashley@semantic.org>
2022-05-04 19:55:51 -0700

    update for witness

--- src/Data/OpenWitness/ST.hs
+++ src/Data/OpenWitness/ST.hs
@@ -21,13 +21,13 @@ import Control.Monad.Fix
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
 import Data.OpenWitness
-import Data.Witness.Specific.WitnessDict
+import Data.Witness.Specific.WitnessOfDict
 import Prelude
 
-type ST s = StateT (WitnessDict (OpenWitness s)) (OW s)
+type ST s = StateT (WitnessOfDict (OpenWitness s)) (OW s)
 
 stToOW :: ST s a -> OW s a
-stToOW st = evalStateT st emptyWitnessDict
+stToOW st = evalStateT st emptyWitnessOfDict
 
 runST :: (forall s. ST s a) -> a
 runST st = runOW (stToOW st)
@@ -41,21 +41,21 @@ newSTRef :: a -> ST s (STRef s a)
 newSTRef a = do
     wit <- lift newOpenWitnessOW
     dict <- get
-    put (witnessDictAdd wit a dict)
+    put (witnessOfDictAdd wit a dict)
     return wit
 
 readSTRef :: STRef s a -> ST s a
 readSTRef key = do
     dict <- get
-    case witnessDictLookup key dict of
+    case witnessOfDictLookup key dict of
         Just a -> return a
         _ -> error "ref not found"
 
 writeSTRef :: forall s a. STRef s a -> a -> ST s ()
-writeSTRef key newa = modify (witnessDictReplace key newa)
+writeSTRef key newa = modify (witnessOfDictReplace key newa)
 
 modifySTRef :: forall s a. STRef s a -> (a -> a) -> ST s ()
-modifySTRef key amap = modify (witnessDictModify key amap)
+modifySTRef key amap = modify (witnessOfDictModify key amap)
 
 stToIO :: ST RealWorld a -> IO a
 stToIO = owToIO . stToOW
